98da9780e51c9642bdcde4586c46fb51b1e2f63c
Pujakumariharh <121793206+Pujakumariharh@users.noreply.github.com>
2023-01-03 13:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonX
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/singing_culb")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/singing_culb")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/alone_support")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/alone_support")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
